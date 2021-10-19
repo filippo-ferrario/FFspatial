9102ba86caa243f9a2e4b76baaca7251c8ac2b49
filippo-ferrario <filippo.f3rrario@gmail.com>
2021-10-19 16:00:30 -0400

    Random point per category of polygons
    This version (0.2) is randomly place the same amount of points in polygons of selected categories. This approach taken because st_sample(by.polygon=T) does not seem to wotk.

--- R/point_quadrat.R
+++ R/point_quadrat.R
@@ -2,7 +2,7 @@
 # Name   : Substrate classification
 # Author : Filippo Ferrario
 # Date   : 13/10/2021 
-# Version: 0.1
+# Version: 0.2
 # Aim    : prepare point and quadrats shapefile to sample the substrate shapefile resulting from segmentation.
 #		   The idea is to characterize the size class distribution of rocks in polygons identified as matrix and those identified as rocks. 
 #           Desired output:
@@ -24,7 +24,7 @@
 #' @param shp polygon shapefile that need to be sampled.
 #' @param site_poly optional. Polygon delimiting the area of the site to be taken into account. If not provided the shapefile provided in 'shp' will be consitered as site.
 #' @param n_quad number of replicate quadrats 
-#' @param n_pts number of points per quadrats
+#' @param n_pts number of points per category per quadrats
 #' @param seed seed for random generation if needed.
 #' @param quadrat_size size of the replicare quadrat in meters.
 #' @param class_lab not used 
@@ -37,23 +37,23 @@ point_quadrat<-function(dsn=NULL,file_tag=NULL,shp=NULL, site_poly=NULL, n_quad=
 {
 
     # require(sf)
-    # require(tidyverse)
+    require(tidyverse)
     # require(sfheadres)
     # set seed if needed
 	# make 1 unique polygon of from the shp
-	if(!is.null(site_poly)) {site<-site_poly } 
-		else {site<-st_union(shp) %>% sfheaders::sf_remove_holes(.)}
+	if(!is.null(site_poly)) {site<-site_poly 
+            } else {site<-sf::st_union(shp) %>% sfheaders::sf_remove_holes(.)}
 		
     # make grid
-    grd<-st_make_grid(site, cellsize=quadrat_size)
+    grd<-sf::st_make_grid(site, cellsize=quadrat_size)
     # make sure the reference system is the same
-    st_crs(grd)<-st_crs(site)
+    sf::st_crs(grd)<-sf::st_crs(site)
 
     # seleziono solo quadrati interamente contenuti nel transetto
-    grd<-st_as_sf(grd)
-    site<-st_as_sf(site)
-    int<- st_intersection(grd,site) %>%
-           mutate(area=as.numeric(st_area(.)) ) %>%
+    grd<-sf::st_as_sf(grd)
+    site<-sf::st_as_sf(site)
+    int<- sf::st_intersection(grd,site) %>%
+           mutate(area=as.numeric(sf::st_area(.)) ) %>%
            filter(area==quadrat_size^2) %>%
            mutate(repl=1:nrow(.))
     
@@ -65,20 +65,30 @@ point_quadrat<-function(dsn=NULL,file_tag=NULL,shp=NULL, site_poly=NULL, n_quad=
         # ID_poly<- as.data.frame(shp) %>% 
         #             .[,class_col] %in% class_val
     
-        # int_quad<- st_intersection(shp[ID_poly,],quad_samp) %>% 
+        # int_quad<- sf::st_intersection(shp[ID_poly,],quad_samp) %>% 
     # }
-    int_quad<- st_intersection(shp,quad_samp) %>% 
-                st_cast(., 'MULTIPOLYGON')
+    int_quad<- sf::st_intersection(shp,quad_samp) %>% 
+                sf::st_cast(., 'MULTIPOLYGON') %>%
+                filter(.data[[class_lab]] %in% class_val )
 
-                 set.seed(988)
-    pt_shp<-lapply(1:n_quad, function(x){
-                      pt_samp<- int_quad[ int_quad$repl== IDs[x],] %>%
-                                st_sample(st_geometry(.), size=n_pts, by_polygon=T) %>%  
-                                st_as_sf(.)   
-                    }) %>% bind_rows(.)
-    st_write(quad_samp, dsn=paste0(dsn,'/',file_tag,'-pq-rep_quadrats.shp'))
-    st_write(pt_shp, dsn=paste0(dsn,'/',file_tag,'-pq-points_quadrats.shp'))
-    st_write(int_quad, dsn=paste0(dsn,'/',file_tag,'-pq-poly_quadrats.shp'))
+    #set.seed(988)
+    pt_shp<-NULL
+    for (i in 1:length(class_val)){            
+        pt_temp<-lapply(1:n_quad, function(x){
+                                    ww<-int_quad %>% #[ int_quad$repl== IDs[x],] %>%
+                                    filter(.data[[class_lab]]==class_val[i], int_quad$repl== IDs[x] ) %>%
+                                    sf::st_sample(sf::st_geometry(.), size=n_pts, by_polygon=F) %>%  
+                                    sf::st_as_sf(.)   
+                        }) %>% bind_rows(.)
+        pt_shp<-rbind(pt_shp,pt_temp)
+
+
+    }
+
+
+    sf::st_write(quad_samp, dsn=paste0(dsn,'/',file_tag,'-pq-rep_quadrats.shp'))
+    sf::st_write(pt_shp, dsn=paste0(dsn,'/',file_tag,'-pq-points_quadrats.shp'))
+    sf::st_write(int_quad, dsn=paste0(dsn,'/',file_tag,'-pq-poly_quadrats.shp'))
 
 
 }
@@ -86,16 +96,25 @@ point_quadrat<-function(dsn=NULL,file_tag=NULL,shp=NULL, site_poly=NULL, n_quad=
 
 
 
-# #=====================
-# # bench data
-# #=====================
-# library(sf)
+# # #=====================
+# # # bench data
+# # #=====================
+# # library(sf)
+# dsn='E:/PPO_godbout/GIS/shapefiles/pt_quad'
+# file_tag='temp'
+# site_poly=NULL 
+# n_quad=3
+# n_pts=6
+# seed=1294
+# quadrat_size=1 
+# class_lab='class'
+# class_val=c( "Bloc ou galet","Sédiment mixte grossier") 
 
 # shp<-sf::st_read('C:/Users/Utilisateur/Documents/Lavoro/DFO/GODBOUT_mosaics/data/validated shp/val_ph03_epsg32619.shp')
-# plot(sf::st_geometry(shp), col=as.factor(shp$class))
-# str(shp)
+# # plot(sf::st_geometry(shp), col=as.factor(shp$class))
+# # str(shp)
 
 
-# plot(st_geometry(int_quad[int_quad$repl==909,]), col=as.factor(int_quad[int_quad$repl==909,]$class), border=2 )
-# plot(st_geometry(pt_shp), add=T, col='white', pch=20)
+# # plot(st_geometry(int_quad[int_quad$repl==909,]), col=as.factor(int_quad[int_quad$repl==909,]$class), border=2 )
+# # plot(st_geometry(pt_shp), add=T, col='white', pch=20)
 
