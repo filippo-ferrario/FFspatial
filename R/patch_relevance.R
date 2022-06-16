b95be40da193970ec7bf52f23c6b23dbb0bb4314
Filippo Ferrario <filippo.f3rrario@gmail.com>
2022-06-16 17:00:11 -0400

    Patch relevanc functions

---
+++ R/patch_relevance.R
@@ -0,0 +1,68 @@
+# ===============================================================================
+# Name   	: Patch Relevance index
+# Author 	: Filippo Ferrario
+# Date   	: 16-06-2022 [dd-mm-yyyy]
+# Version	: 0.1
+# URL		: 
+# Aim    	: 
+# ===============================================================================
+
+#' Patch Relevance index calculated within a buffer.
+#' 
+#' @param cx an st_point center of the buffer
+#' @param poly a collection of polygons representing the patches to be use to calculate the index
+#' @param buffer the radius of the buffer to use around `cx`.
+#' @param one_body_lenght the lenght to be used to standardize the distance: e.g. the mean length or the radius of the body for a given species.
+#' @param zero_distance the distance value that will be used to calculate the Area:distance ratio when `cx` falls in a patch. All distances < than `zero_distance` will be set to this.
+#' 
+#' @details   
+#' 
+#' The `PRindex` is calculated as:
+#' $$PR = \sum_{n=1}^{n} \frac{A}{D}$$
+#' 
+#' The `PRindex_body` is calculated as:
+#' $$SR = \sum_{n=1}^{n} \frac{A}{\frac{D}{mean~~body~~lenght}}$$
+#' 
+#' where _A_ is the area of the _i_ polygon in a buffer of a given radius, and _D_ is the distance of that polygon to the center of the buffer.
+#' 
+#' 
+#' @author Filippo Ferrario, \email{filippo.f3rrario@gmail.com} 
+#' 
+#' @examples 
+#' 
+#' @seealso
+#' [sf::st_point], [sf::st_buffer]
+#' 
+#' 
+#' @export 
+
+
+
+
+
+#' @describeIn PRindex
+PRindex_body<-function(cx, poly, one_body_length=0.03, buffer=5){
+	# inID<-which(st_contains(cx,poly, sparse=FALSE)==T)
+	buff<-st_buffer(cx, dist=buffer)
+	poly<-st_intersection(poly,buff)
+	area<-st_area(poly)
+	bdlt<-st_distance(cx,poly,sparse=FALSE)/one_body_length
+	bdlt[bdlt>0 & bdlt<1]<-1
+	bdlt[bdlt==0]<-0.5
+	res<-sum(area/bdlt)
+	res
+}
+
+#' @describeIn PRindex
+#' @export 
+# based only on distance
+PRindex<-function(cx, poly, buffer=5, zero_distance=0.01){
+	buff<-st_buffer(cx, dist=buffer)
+	poly<-st_intersection(poly,buff)
+	area<-st_area(poly)
+	bdlt<-st_distance(cx,poly)
+	bdlt[bdlt<zero_distance]<-zero_distance # arbitrairily choose as zero_distance. all distances which are less than zero_distance are set to this value.
+	# bdlt[bdlt>0 & bdlt<1]<-1
+	res<-sum(area/bdlt)
+	res
+}
