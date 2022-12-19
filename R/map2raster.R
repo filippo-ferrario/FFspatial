61322ba2bdde70fce1a37f3451068b4c86fadc2a
Filippo Ferrario <filippo.f3rrario@gmail.com>
2022-12-19 18:22:27 -0500

    changed call to FUN
    changed call to FUN to allow more flexibility in arguments passed to FUN

--- R/map2raster.R
+++ R/map2raster.R
@@ -41,6 +41,10 @@ map2raster<-function(obs_polygon, side.cell=0.1, radius=1, FUN,...) {
 	if (length(obs_poly)!=1) stop('To use this function for more than 1 obs_polygon please use an apply synthax')
 	# if (!is.function(FUN)) stop("argument FUN is not a function!")
 	
+	# List of Optional arguments
+	listArgs<-list(...)
+	
+
 	# define points for which calculate the index
 	regrd<-st_make_grid(obs_poly, cellsize=side.cell, square=TRUE)
 	pixID<-st_intersects(regrd,obs_poly, sparse=FALSE)
@@ -49,14 +53,19 @@ map2raster<-function(obs_polygon, side.cell=0.1, radius=1, FUN,...) {
 	cx<-st_centroid(regrd)
 	# create buffers per each point
 	buffer<-st_buffer(cx, dist=radius)
+	# add buffer and cx to list of optional arguments to be passed to FUN
+	listArgs$cx<-cx
+	listArgs$buffer<-buffer
 	# METRIC BLOCK
 	# ------------------
-		res_vect<-FUN(buffer,cx, ...)
+		res_vect<-do.call(FUN, args=listArgs)  # do.call allows to interpret the listArgs with amended arguments
 	# ---------------------------
 	# Now extract the coordinates from the point geometry and combine them with the values.
         dt2<-cbind(st_coordinates(cx),res_vect)
 		# create a raster from XYZ
 		ras<-raster::rasterFromXYZ(dt2, res=c(side.cell,side.cell))
+		# add CRS
+		raster::crs(ras)<-raster::crs(obs_poly)
 		ras
 
 }
@@ -72,11 +81,11 @@ map2raster<-function(obs_polygon, side.cell=0.1, radius=1, FUN,...) {
 # rcksf<-sf::st_cast(sbs_vect[sbs_vect$class_corrected=='rock',], 'POLYGON')
 
 
-# newOBS<-st_buffer(st_centroid(trssf[1,]), dist=2)
+newOBS<-st_buffer(st_centroid(trssf[1,]), dist=2)
 
 
 # system.time(
-# res<-map2raster(obs_polygon=newOBS, side.cell=0.5, radius=1, FUN=PatchRelv, zero_distance=0.01,poly=rcksf)
+res<-map2raster(obs_polygon=newOBS, side.cell=0.5, radius=1, FUN=PatchRelv, zero_distance=0.5,poly=rcksf)
 # )
 
 
